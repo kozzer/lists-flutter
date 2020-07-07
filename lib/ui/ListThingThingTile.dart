1d56053832fa9bc4d2bb380a3a0277fe7cca6b4c
David Kozlowski <kozzer32@hotmail.com>
2020-07-06 19:34:17 -0500

    Removed unused import

--- lib/ui/ListThingThingTile.dart
+++ lib/ui/ListThingThingTile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:lists/models/ListThing.dart';
 import 'package:lists/models/ListsScopedModel.dart';
 import 'package:lists/ui/ListThingEntry.dart';
-import 'package:scoped_model/scoped_model.dart';
 
 /// Custom ListTile class for things that are lists
 class ListThingThingTile extends StatelessWidget {
